4f7fc024ae59e04bee54c72f60741de97780de96
felzosqrt <maximsessler@mail.de>
2024-01-30 16:33:51 +0100

    firebase eingerichtet

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
-
+import 'package:alertnukeapp/screens/login/login.dart';
 import 'package:alertnukeapp/screens/wrapper.dart';
 import 'package:alertnukeapp/screens/social/social.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( MaterialApp(
-    //runapp ist in der Flutter Lib definiert
-    home: Root(),
-  ));
+void main() async {
+  // Initialisiere Firebase
+  WidgetsFlutterBinding.ensureInitialized(); // Erforderlich für Firebase-Initialisierung
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Starte die Flutter-App
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //UsermodelRepository 
+      //When userdaten 
+      home: LoginScreen(), // Hier kannst du den Wrapper oder eine andere Startseite festlegen
+    );
+  }
+}
+
+
+////firebase@1.com  Peterpan123!
